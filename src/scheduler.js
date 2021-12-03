9410a5541b31f5656c7a4dbe67174b9d868fea48
Waddah Mortada <w.mortada87@gmail.com>
2021-12-03 15:04:34 +0000

    Refactor Scheduler (WIP)

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,23 +1,45 @@
-const cron = require('node-cron')
+import cron from 'node-cron'
 
-const scheduler = (interval, time) => {
-  console.log('interval', interval)
-  console.log('time', time)
+const tasks = {}
 
-  // Runs every weekday (Monday through Friday) at 11:30:00 AM. It does not run on Saturday or Sunday.
-  cron.schedule('00 00 07 * * 1-5', () => {
-    console.log('running a task every minute')
+// Runs every weekday (Monday through Friday) at 07:00:00 AM. It does not run on Saturday or Sunday.
+cron.schedule('00 00 07 * * 1-5', () => {
+  console.log('running a task every minute')
+})
+
+const onceAWeek = callback => {
+  const task = cron.schedule('0 0 0 * * 7', () => {
+    callback()
   })
+  tasks[timeString] = task
+}
 
-  const onceAWeek = (callback) => {
-    cron.schedule('0 0 0 * * 7', () => {
-      callback()
-    })
-  }
-
+const convertTimeToObject = time => {
+  const timeArr = time.split(':')
   return {
-    onceAWeek
+    hour: timeArr[0],
+    minute: timeArr[1]
   }
 }
 
-export default scheduler
+const weekday = (timeString, callback) => {
+  console.log(`Scheduling a weekday message Time ${timeString}`)
+  const time = convertTimeToObject(timeString)
+  const task = cron.schedule(`0 ${time.minute} ${time.hour} * * 1-5`, () => {
+    console.log('Excuting timeString task')
+    callback()
+  })
+  tasks[timeString] = task
+}
+
+const cancel = time => {
+  console.log(`Cancel a schedule message Time ${time}`)
+  tasks[time].stop()
+  delete tasks[time]
+}
+
+export default {
+  onceAWeek,
+  weekday,
+  cancel
+}
